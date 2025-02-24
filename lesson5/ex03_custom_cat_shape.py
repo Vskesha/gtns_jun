157b1720b27c48047fc3dbce309ea6cf90046c47
vskesha <vskesha@gmail.com>
2025-02-25 00:49:04 +0200

    changes in getting image filename

--- lesson5/ex03_custom_cat_shape.py
+++ lesson5/ex03_custom_cat_shape.py
@@ -1,10 +1,15 @@
 import turtle
+from pathlib import Path
 
 screen = turtle.Screen()
 t = turtle.Turtle()
 
-screen.addshape("cat_cry.gif")
-t.shape("cat_cry.gif")
+image_name = "cat_cry.gif"
+print(Path(__file__))
+image_path = str(Path(__file__).parent.joinpath(image_name)) 
+
+screen.addshape(image_path)
+t.shape(image_path)
 t.forward(100)
 
 turtle.done()
