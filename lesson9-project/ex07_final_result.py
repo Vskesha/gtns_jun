20c976ee72d8283e582ebf6d88859ffc9da75ea7
vskesha <vskesha@gmail.com>
2025-03-23 20:25:48 +0200

    template

---
+++ lesson9-project/ex07_final_result.py
@@ -0,0 +1,19 @@
+import turtle
+
+# Настройки програми
+
+
+screen = turtle.Screen()
+
+# Функція для початку гри
+def start_game(x, y):
+    screen.clear()
+    t = turtle.Turtle()
+
+# Функція для малювання кнопки "Почати гру"
+
+
+# Відслідковування натискання на кнопку
+
+
+turtle.done()
